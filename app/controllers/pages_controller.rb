e9f70775dbdad708710adf9c1806f8b96298a2a8
Florian Guerin <guerinflorian02@gmail.com>
2023-06-27 15:13:32 +0200

    navbar done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,7 @@ class PagesController < ApplicationController
 
   def about_me
   end
+
+  def project
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root 'pages#home'
   get '/about-me', to: 'pages#about_me'
+  get '/projects', to: 'pages#project'
 end
